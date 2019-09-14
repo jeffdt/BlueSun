fac3798926f5fc8f8220266c0a778f352808c037
jeffdt <1211867+jeffdt@users.noreply.github.com>
2019-09-14 19:36:56 -0400

    Add sprite batch extension for drawing pixel perfect

---
+++ PigeonEngine/utilities/extensions/SpriteBatchExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigeonEngine.utilities.extensions {
+    public static class SpriteBatchExtensions {
+        public static void BeginPixelPerfect(this SpriteBatch spriteBatch, Matrix transformMatrix) {
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, transformMatrix);
+        }
+    }
+}
