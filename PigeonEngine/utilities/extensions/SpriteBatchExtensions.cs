206a1962be043d979cd4a525ea5ae37710e1583d
jeffdt <1211867+jeffdt@users.noreply.github.com>
2019-09-14 20:05:54 -0400

    Add sorting mode to sprite batch extension

--- PigeonEngine/utilities/extensions/SpriteBatchExtensions.cs
+++ PigeonEngine/utilities/extensions/SpriteBatchExtensions.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PigeonEngine.utilities.extensions {
     public static class SpriteBatchExtensions {
-        public static void BeginPixelPerfect(this SpriteBatch spriteBatch, Matrix transformMatrix) {
-            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, transformMatrix);
+        public static void BeginPixelPerfect(this SpriteBatch spriteBatch, Matrix transformMatrix, SpriteSortMode sortMode = SpriteSortMode.Deferred) {
+            spriteBatch.Begin(sortMode, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, transformMatrix);
         }
     }
 }
