de0ec40d57228c4351472575dcd953477043c49d
jeffdt <1211867+jeffdt@users.noreply.github.com>
2019-10-13 12:00:40 -0400

    Merge from master

--- PigeonEngine.Tests/utilities/extensions/StringTextensionsTest.cs
+++ PigeonEngine.Tests/utilities/extensions/StringTextensionsTest.cs
@@ -190,7 +190,7 @@ namespace PigeonEngineTests.utilities.extensions {
         }
 
         [TestCase("a b c", new string[] { "a", "b", "c" })]
-        [TestCase("a b cde fgh", new string[] { "a", "b", "cde", "fg h" })]
+        [TestCase("a b cde fgh", new string[] { "a", "b", "cde", "fgh" })]
         public void Tokenize(string input, string[] expected) {
             Assert.AreEqual(expected, input.Tokenize());
         }
